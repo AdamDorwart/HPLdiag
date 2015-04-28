2242a4bf51d41de0e977031a8b81236ff9f34c5d
Adam Dorwart <adorwart@ucsd.edu>
2015-04-28 09:33:37 -0700

    Cleaned up output, added spinner while setting up jobs.

--- HPLdiag.sh
+++ HPLdiag.sh
@@ -2,11 +2,16 @@
 postfix=`date +'%m%d%y'`
 echo "Starting HPLmon diagnostics on Comet (`date`)" >> log/HPLresults.$postfix
 echo "-----------------------------------------------------------" >> log/HPLresults.$postfix
+spinq="/-\|"
 while read line; do
 	node=`echo $line | awk '{ print $3}'`
-	sbatch -w $node hpl_1n_12p_2t.comet $postfix
+	sbatch -w $node hpl_1n_12p_2t.comet $postfix > /dev/null
+	spinf=${spinq#?}
+	printf "Dispatching jobs [%c] " "$spinq"
+	spinq=$spinf${spinq%"$spinf"}
+	printf "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"
 done < $1
-squeue -o "%R %i %n" -u adorwart | grep "ReqNodeNotAvail" | while read line; do 
+squeue -o "%R %i %n" -u $USER | grep "ReqNodeNotAvail" | while read line; do 
 	DOAjobid=`echo $line | awk '{ print $2}'`
 	DOAnode=`echo $line | awk '{ print $3}'`
 	scancel $DOAjobid
