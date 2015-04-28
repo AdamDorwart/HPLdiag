b5aa206f2436966cc44edddc20f96673a8a9466b
Adam Dorwart <adorwart@ucsd.edu>
2015-04-28 08:47:19 -0700

    Lots of updates. Already a fully functional test of Comet. Just need to include instructions/installer to setup cronjob

--- HPLdiag.sh
+++ HPLdiag.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-time=`date +'%m%d%y'`
+postfix=`date +'%m%d%y'`
+echo "Starting HPLmon diagnostics on Comet (`date`)" >> log/HPLresults.$postfix
+echo "-----------------------------------------------------------" >> log/HPLresults.$postfix
 while read line; do
 	node=`echo $line | awk '{ print $3}'`
-	sbatch -w $node hpl_1n_12p_2t.comet $time
+	sbatch -w $node hpl_1n_12p_2t.comet $postfix
 done < $1
+squeue -o "%R %i %n" -u adorwart | grep "ReqNodeNotAvail" | while read line; do 
+	DOAjobid=`echo $line | awk '{ print $2}'`
+	DOAnode=`echo $line | awk '{ print $3}'`
+	scancel $DOAjobid
+	echo "$DOAjobid $DOAnode UNAVAILABLE" >> log/HPLresults.$postfix
+done
+
