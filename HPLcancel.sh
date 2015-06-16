25d27c5b2e24cd32dd9c55d1c6c9872377db3f6a
Adam Dorwart <adorwart@ucsd.edu>
2015-06-16 14:14:47 -0700

    Continued refactoring tests structure and imporved handling of edge cases

--- HPLcancel.sh
+++ HPLcancel.sh
@@ -2,11 +2,18 @@
 #
 # Cancels all running jobs for the current user and logs the action
 
+# Set to false to cancel all queued jobs
+#RUNNING_JOBS=false
+
 postfix=`date +'%m%d%y'`
-squeue --format="%R %i" -u $USER | grep "comet" | while read line; do 
+queuedJobs=`squeue --format="%n %i %R" --noheader --user=$USER`
+#if [ "$RUNNING_JOBS" == true ]; then
+#	queuedJobs=`echo "$queuedJobs" | grep "comet"`
+#fi
+echo "$queuedJobs" | while read line; do 
 	DOAjobid=`echo $line | awk '{ print $2}'`
 	DOAnode=`echo $line | awk '{ print $1}'`
-	scancel $DOAjobid
+	scancel $DOAjobid > /dev/null
 	echo "$DOAjobid $DOAnode canceled before completion" >> log/HPLresults.$postfix
 done
 
--- HPLdiag.sh
+++ HPLdiag.sh
@@ -23,21 +23,23 @@ while read line; do
 	# Dispatch job based on the partition
 	# Default to compute partition if none given
 	if [ "$partition" == "compute" -o "$partition" == "shared" ]; then
-		sbatch --nodelist=$node --partition=$partition hpl_1n_12p_2t.comet $postfix > /dev/null
+		sbatch --nodelist=$node --partition=$partition CPU/hpl_1n_12p_2t.comet $postfix > /dev/null
 	elif [ "$partition" == "gpu" -o "$partition" == "gpu-shared" ]; then
-		sbatch --nodelist=$node --partition=$partition hpl_1n_12p_2t.comet $postfix > /dev/null
-		# Need AMBER GPU tests as well as CPU/GPU HPL tests
+		# Run both CPU and GPU tests on GPU nodes
+		sbatch --nodelist=$node --partition=$partition CPU/hpl_1n_12p_2t.comet $postfix > /dev/null
+		#TODO: Need AMBER GPU tests as well as CPU/GPU HPL tests
 	elif [ "$partition" == "" ]; then
-		sbatch --nodelist=$node --partition="compute" hpl_1n_12p_2t.comet $postfix > /dev/null
+		sbatch --nodelist=$node --partition="compute" CPU/hpl_1n_12p_2t.comet $postfix > /dev/null
 	fi
 	update_spinner
 done < $1
 # Cancel all jobs for nodes that are down, failed, or in maintenece
-sinfo --format="%n %T %E" | grep -e "maint" -e "down" -e "drained" -e "fail" | while read line; do
+queued_nodes=`squeue -h -o %n -u $USER | sed ':a;N;$!ba;s/\n/,/g'`
+sinfo --format="%n %T %E" --nodes="$queued_nodes" --noheader | sort -u | grep -e "maint" -e "down" -e "drained" -e "fail" | while read line; do
 	DOAnode=`echo $line | awk '{print $1}'`
 	DOAjobid=`squeue --format="%i %n" -u $USER | grep $DOAnode | awk '{print $1}'`
 	if [ -n $DOAjobid ]; then	
-		scancel $DOAjobid
+		scancel $DOAjobid &> /dev/null
 		echo "$DOAjobid $line" >> log/HPLresults.$postfix
 	fi
 done
